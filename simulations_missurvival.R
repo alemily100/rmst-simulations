820a0566bbc54ec4dcf129e2914bd66a164579f0
Emily Alger <118363157+alemily100@users.noreply.github.com>
2024-07-16 12:49:59 +0100

    Add files via upload

--- simulations_missurvival.R
+++ simulations_missurvival.R
@@ -1,16 +1,5 @@
-library(survival)
-library(tidyverse)
-library(survminer)
-library(Rlab)
-library(calculus)
-library(dplyr)
-library(eha)
-library(rgl)
-library(survRM2)
-library(parallel
-        )
-source(functions_interaction.R)
-
+setwd("~/Library/CloudStorage/OneDrive-TheAlanTuringInstitute/RMST_paper")
+source("functions_missurvival.R")
 #######################RUN BEFORE EVERY CALCULATION#############################
 #Before each power/type I error calculation, re-run this code chunk to ensure 
 #all parameters are reset to original values   
@@ -29,87 +18,87 @@ fit3 <- phreg(Surv(time, status) ~ treat
 treat.coef<- fit3$coefficients[1]
 inherit.coef<- fit3$coefficients[2]
 treat.inherit.coef<- fit3$coefficients[4]
-sex.coef<- fit3$coefficients[3]
+#sex.coef<- fit3$coefficients[3]
+sex.coef<- -1
 base.hazard1<- exp(-fit3$coefficients[5])
 cens<- 0.001
 arrival<-20
 end<-120
-
-N<-100000
-sample<-100
 base_haz<- base.hazard1
 vec_cov<-c(treat.coef, inherit.coef, treat.inherit.coef, sex.coef)
 
+N<- 10000
+sample<-100
 
 ############################POWER CALCULATIONS##################################
 ################################################################################
 
+
 ################################VARYING t*######################################
-cl <- makeCluster(detectCores())
+cl <- makeCluster(detectCores()-1)
+clusterSetRNGStream(cl,1999)
 invisible(clusterEvalQ(cl,{
   library(survival)
   library(tidyverse)
   library(survminer)
-  library(Rlab)
+  #library(Rlab)
   library(calculus)
   library(dplyr)
   library(eha)
-  library(rgl)
+  #library(rgl)
   library(survRM2)
   library(parallel)
-  setwd("~/MRC/FINAL")
-  source("functions.r")
+  setwd("~/Library/CloudStorage/OneDrive-TheAlanTuringInstitute/RMST_paper")
+  source("functions_missurvival.R")
 }))
 clusterExport(cl, c("sample", "base_haz", "vec_cov", "cens",
                     "arrival", "end"))
 result.data <- data.frame(t_star=NULL,
-                          nonparam.power=NULL, missparam.power=NULL, param.power=NULL)
+                          nonparam.power=NULL, param.power=NULL, missparam.power=NULL, cox.power=NULL)
 stat.data <- data.frame(t_star=NULL,
                           nonparam.z=NULL,nonparam.se=NULL, nonparam.te=NULL,
                         fullparam.z=NULL, fullparam.se=NULL, fullparam.te=NULL,
                         missparam.z=NULL, missparam.se=NULL, missparam.te=NULL)
-t<-seq(from=1, to=150, length.out=15)
-error<-c()
+t<-seq(from=1, to=120, length.out=20)
 large_var<-c()
 no_z<-c()
 start_time<-Sys.time()
 for(i in 1:length(t)){
+  #t_star<-t[i]
   t_star<-t[i]
   clusterExport(cl, c("t_star"))
   val<-parSapply(cl, rep(t_star, times=N),  function(k) generate_z_values(sample, base_haz, vec_cov, cens, arrival, end, k))
-  result.data <- rbind(result.data, data.frame(t_star=t_star, nonparam.power=sum(val[1,]>qnorm(0.975),na.rm=TRUE)/(N-sum(is.na(val[1,]))),param.power=sum(val[4,]>qnorm(0.975))/N, missparam.power=sum(val[7,]>qnorm(0.975))/N))
-  val_subset<-val[,val[6,]<1000]
-  stat.data<- rbind(stat.data, data.frame(t_star=t_star, nonparam.z=mean(val_subset[1,], na.rm=TRUE),nonparam.se=mean(val_subset[3,],na.rm = TRUE), nonparam.te=mean(val_subset[2,],na.rm=TRUE),
-                                          fullparam.z=mean(val_subset[4,]), fullparam.se=mean(val_subset[6,]), fullparam.te=mean(val_subset[5,]),
-                                          missparam.z=mean(val_subset[7,]), missparam.se=mean(val_subset[9,]), missparam.te=mean(val_subset[8,])))
-  no_z[i]<-sum(is.na(val[1,]))
-  large_var[i]<-sum(val[6,]>=1000)
+  n<- N-sum(is.na(val[4,]))
+  te<- data.frame(val[1,], val[2,], val[3,], val[4,], val[5,], val[6,], val[7,], val[8,], val[9,])
+  te[,10]<- rep(t_star, times =nrow(te))
+  colnames(te)<-c("npz","npt","nps", "fpz","fpt", "fps", "mpz", "mpt", "mps","t")
+  
+  result.data <- rbind(result.data, data.frame(t_star=t_star, nonparam.power=sum(val[1,]>qnorm(0.975),na.rm=TRUE)/n,param.power=sum(val[4,]>qnorm(0.975),na.rm=TRUE)/n, missparam.power=sum(val[7,]>qnorm(0.975),na.rm=TRUE)/n, 
+                                               cox.power=sum(val[10,]<qnorm(0.025),na.rm=TRUE)/n))
+  no_z[i]<-N-n
+  write.csv(te, paste0(i, "tpbreak.csv"))
 }
 stopCluster(cl)
-end_time<-Sys.time()
-total_time<-end_time-start_time
-
-write.csv(result.data, "power_varyt_star.csv")
-write.csv(stat.data, "power_varyt_starbreakdown.csv")
-write.csv(large_var, "power_varyt_starlargevariance.csv")
-write.csv(no_z, "power_varyt_star_nonparamnotpossible.csv")
+write.csv(result.data, "tp.csv")
+write.csv(no_z, "tpimpossible.csv")
 
 ################################VARYING COV_3###################################
 t_star<-100
 cl <- makeCluster(detectCores()-1)
+clusterSetRNGStream(cl,1999)
 invisible(clusterEvalQ(cl,{
   library(survival)
   library(tidyverse)
   library(survminer)
-  library(Rlab)
+  #library(Rlab)
   library(calculus)
   library(dplyr)
   library(eha)
-  library(rgl)
+  #library(rgl)
   library(survRM2)
   library(parallel)
-  setwd("~/MRC/FINAL")
-  source("functions.r")
+  setwd("~/Library/CloudStorage/OneDrive-TheAlanTuringInstitute/RMST_paper")
+  source("functions_missurvival.R")
 }))
 clusterExport(cl, c("sample", "treat.coef", "inherit.coef", "treat.inherit.coef", "base_haz", "cens",
                     "arrival", "end", "t_star"))
@@ -119,7 +108,7 @@ stat.data <- data.frame(t_star=NULL,
                         nonparam.z=NULL,nonparam.se=NULL, nonparam.te=NULL,
                         fullparam.z=NULL, fullparam.se=NULL, fullparam.te=NULL,
                         missparam.z=NULL, missparam.se=NULL, missparam.te=NULL)
-b<-seq(from=-2, to=2, length.out=10)
+b<-seq(from=-2, to=2, length.out=20)
 large_var<-c()
 no_z<-c()
 start_time<-Sys.time()
@@ -128,21 +117,18 @@ for(i in 1:length(b)){
   clusterExport(cl, c("beta3"))
   start_time <- Sys.time()
   val<-parSapply(cl, rep(beta3, times=N),  function(k) generate_z_values(sample, base_haz, c(treat.coef, inherit.coef, treat.inherit.coef, k), cens, arrival, end, t_star))
-  result.data <- rbind(result.data, data.frame(beta3=beta3, nonparam.power=sum(val[1,]>qnorm(0.975),na.rm=TRUE)/(N-sum(is.na(val[1,]))),param.power=sum(val[4,]>qnorm(0.975),na.rm=TRUE)/N, missparam.power=sum(val[7,]>qnorm(0.975),na.rm=TRUE)/N))
-  val_subset<-val[,val[6,]<1000]
-  stat.data<- rbind(stat.data, data.frame(beta3=beta3, nonparam.z=mean(val_subset[1,], na.rm=TRUE),nonparam.se=mean(val_subset[3,],na.rm=TRUE), nonparam.te=mean(val_subset[2,],na.rm=TRUE),
-                                          fullparam.z=mean(val_subset[4,]), fullparam.se=mean(val_subset[6,]), fullparam.te=mean(val_subset[5,]),
-                                          missparam.z=mean(val_subset[7,]), missparam.se=mean(val_subset[9,]), missparam.te=mean(val_subset[8,])))
-  large_var[i]<-sum(val[6,]>=1000)
-  no_z[i]<-sum(is.na(val[1,]))
+  te<- data.frame(val[1,], val[2,], val[3,], val[4,], val[5,], val[6,], val[7,], val[8,], val[9,])
+  te[,10]<- rep(t_star, times =nrow(te))
+  colnames(te)<-c("npz","npt","nps", "fpz","fpt", "fps", "mpz", "mpt", "mps","t")
+  n<- N-sum(is.na(val[4,]))
+  result.data <- rbind(result.data, data.frame(beta3=beta3, nonparam.power=sum(val[1,]>qnorm(0.975),na.rm=TRUE)/n,param.power=sum(val[4,]>qnorm(0.975),na.rm=TRUE)/n, missparam.power=sum(val[7,]>qnorm(0.975),na.rm=TRUE)/n,
+                                               cox.power=sum(val[10,]<qnorm(0.025), na.rm=TRUE)/n))
+  no_z[i]<-N-n
+  write.csv(te, paste0(i, "bpbreak.csv"))
   }
-end_time<-Sys.time()
-total_time<-end_time-start_time
 
-write.csv(result.data, "power_varycov_3.csv")
-write.csv(stat.data, "power_varycov_3breakdown.csv")
-write.csv(large_var, "power_varycov_3largevariance.csv")
-write.csv(no_z, "power_varycov3_nonparamnotpossible.csv")
+write.csv(result.data, "bp.csv")
+write.csv(no_z, "bpimpossible.csv")
 
 ###########################TYPE I ERROR CALCULATIONS############################
 ################################################################################
@@ -150,30 +136,31 @@ write.csv(no_z, "power_varycov3_nonparamnotpossible.csv")
 ##############################Varying t^*#######################################
 vec_cov<-c(0, inherit.coef, 0, sex.coef)
 
-cl <- makeCluster(detectCores())
+cl <- makeCluster(detectCores()-1)
+clusterSetRNGStream(cl,1999)
 invisible(clusterEvalQ(cl,{
   library(survival)
   library(tidyverse)
   library(survminer)
-  library(Rlab)
+  #library(Rlab)
   library(calculus)
   library(dplyr)
   library(eha)
-  library(rgl)
+  #library(rgl)
   library(survRM2)
   library(parallel)
-  setwd("~/MRC/FINAL")
-  source("functions.r")
+  setwd("~/Library/CloudStorage/OneDrive-TheAlanTuringInstitute/RMST_paper")
+  source("functions_missurvival.R")
 }))
 clusterExport(cl, c("sample", "base_haz", "vec_cov", "cens",
                     "arrival", "end"))
 result.data <- data.frame(t_star=NULL,
-                          nonparam.power=NULL, missparam.power=NULL, param.power=NULL)
+                          nonparam.power=NULL, param.power=NULL, missparam.power=NULL, cox.power=NULL)
 stat.data <- data.frame(t_star=NULL,
                         nonparam.z=NULL,nonparam.se=NULL, nonparam.te=NULL,
                         fullparam.z=NULL, fullparam.se=NULL, fullparam.te=NULL,
                         missparam.z=NULL, missparam.se=NULL, missparam.te=NULL)
-t<-seq(from=0, to=150, length.out=15)
+t<-seq(from=1, to=120, length.out=20)
 error<-c()
 large_var<-c()
 no_z<-c()
@@ -182,24 +169,21 @@ for(i in 1:length(t)){
   t_star<-t[i]
   clusterExport(cl, c("t_star"))
   val<-parSapply(cl, rep(t_star, times=N),  function(k) generate_z_values(sample, base_haz, vec_cov, cens, arrival, end, k))
-  #result.data <- rbind(result.data, data.frame(t_star=t_star, nonparam.power=sum(val[1,]>qnorm(0.975))/N,param.power=sum(val[2,]>qnorm(0.975))/N, missparam.power=sum(val[3,]>qnorm(0.975))/N))
-  result.data <- rbind(result.data, data.frame(t_star=t_star, nonparam.power=sum(val[1,]>qnorm(0.975), na.rm = TRUE)/(N-sum(is.na(val[1,]))),param.power=sum(val[4,]>qnorm(0.975))/N, missparam.power=sum(val[7,]>qnorm(0.975))/N))
-  val_subset<-val[,val[6,]<1000]
-  stat.data<- rbind(stat.data, data.frame(t_star=t_star, nonparam.z=mean(val_subset[1,], na.rm=TRUE),nonparam.se=mean(val_subset[3,], na.rm=TRUE), nonparam.te=mean(val_subset[2,], na.rm=TRUE),
-                                          fullparam.z=mean(val_subset[4,]), fullparam.se=mean(val_subset[6,]), fullparam.te=mean(val_subset[5,]),
-                                          missparam.z=mean(val_subset[7,]), missparam.se=mean(val_subset[9,]), missparam.te=mean(val_subset[8,])))
-  no_z[i]<-sum(is.na(val[1,]))
-  large_var[i]<-sum(val[6,]>=1000)
+  te<- data.frame(val[1,], val[2,], val[3,], val[4,], val[5,], val[6,], val[7,], val[8,], val[9,])
+  te[,10]<- rep(t_star, times =nrow(te))
+  colnames(te)<-c("npz","npt","nps", "fpz","fpt", "fps", "mpz", "mpt", "mps","t")
+  n<-N-sum(is.na(val[4,]))
+  result.data <- rbind(result.data, data.frame(t_star=t_star, nonparam.power=sum(val[1,]>qnorm(0.975), na.rm = TRUE)/n, param.power=sum(val[4,]>qnorm(0.975), na.rm=TRUE)/n, missparam.power=sum(val[7,]>qnorm(0.975), na.rm=TRUE)/n, 
+                                               cox.power=sum(val[10,]<qnorm(0.025), na.rm=TRUE)/n))
+  no_z[i]<-N-n
+  write.csv(te, paste0(i, "tt1break.csv"))
 }
 stopCluster(cl)
 end_time<-Sys.time()
 total_time<-end_time-start_time
 
-write.csv(result.data, "type1_varyt_star.csv")
-write.csv(stat.data, "type1_varyt_starbreakdown.csv")
-write.csv(large_var, "type1_varyt_starlargevariance.csv")
-write.csv(no_z, "type1_vary_t_star_nonparamnotpossible.csv")
-
+write.csv(result.data, "tt1.csv")
+write.csv(no_z, "tt1impossible.csv")
 
 ################################Varying beta3###################################
 treat.coef<- 0
@@ -208,29 +192,26 @@ treat.inherit.coef<- 0
 t_star<-100
 
 cl <- makeCluster(detectCores()-1)
+clusterSetRNGStream(cl,1999)
 invisible(clusterEvalQ(cl,{
   library(survival)
   library(tidyverse)
   library(survminer)
-  library(Rlab)
+  #library(Rlab)
   library(calculus)
   library(dplyr)
   library(eha)
-  library(rgl)
+  #library(rgl)
   library(survRM2)
   library(parallel)
-  setwd("~/MRC/FINAL")
-  source("functions.r")
+  setwd("~/Library/CloudStorage/OneDrive-TheAlanTuringInstitute/RMST_paper")
+  source("functions_missurvival.R")
 }))
 clusterExport(cl, c("sample", "treat.coef", "inherit.coef", "treat.inherit.coef", "base_haz", "cens",
                     "arrival", "end", "t_star"))
 result.data <- data.frame(beta3=NULL,
-                          nonparam.power=NULL, missparam.power=NULL)
-stat.data <- data.frame(t_star=NULL,
-                        nonparam.z=NULL,nonparam.se=NULL, nonparam.te=NULL,
-                        fullparam.z=NULL, fullparam.se=NULL, fullparam.te=NULL,
-                        missparam.z=NULL, missparam.se=NULL, missparam.te=NULL)
-b<-seq(from=-2, to=2, length.out=10)
+                          nonparam.power=NULL, param.power=NULL, missparam.power=NULL)
+b<-seq(from=-2, to=2, length.out=20)
 large_var<-c()
 no_z<-c()
 start_time<-Sys.time()
@@ -239,18 +220,14 @@ for(i in 1:length(b)){
   clusterExport(cl, c("beta3"))
   start_time <- Sys.time()
   val<-parSapply(cl, rep(beta3, times=N),  function(k) generate_z_values(sample, base_haz, c(treat.coef, inherit.coef, treat.inherit.coef, k), cens, arrival, end, t_star))
-  result.data <- rbind(result.data, data.frame(beta3=beta3, nonparam.power=sum(val[1,]>qnorm(0.975), na.rm=TRUE)/(N-sum(is.na(val[1,]))),param.power=sum(val[4,]>qnorm(0.975))/N, missparam.power=sum(val[7,]>qnorm(0.975))/N))
-  val_subset<-val[,val[6,]<1000]
-  stat.data<- rbind(stat.data, data.frame(beta3=beta3, nonparam.z=mean(val_subset[1,], na.rm=TRUE),nonparam.se=mean(val_subset[3,], na.rm=TRUE), nonparam.te=mean(val_subset[2,], na.rm=TRUE),
-                                          fullparam.z=mean(val_subset[4,]), fullparam.se=mean(val_subset[6,]), fullparam.te=mean(val_subset[5,]),
-                                          missparam.z=mean(val_subset[7,]), missparam.se=mean(val_subset[9,]), missparam.te=mean(val_subset[8,])))
-  large_var[i]<-sum(val[6,]>=1000)
-  no_z[i]<-sum(is.na(val[1,]))
+  te<- data.frame(val[1,], val[2,], val[3,], val[4,], val[5,], val[6,], val[7,], val[8,], val[9,])
+  te[,10]<- rep(t_star, times =nrow(te))
+  colnames(te)<-c("npz","npt","nps", "fpz","fpt", "fps", "mpz", "mpt", "mps","t")
+  n<- N-sum(is.na(val[4,]))
+  result.data <- rbind(result.data, data.frame(beta3=beta3, nonparam.power=sum(val[1,]>qnorm(0.975), na.rm=TRUE)/n,param.power=sum(val[4,]>qnorm(0.975), na.rm=TRUE)/n, missparam.power=sum(val[7,]>qnorm(0.975), na.rm=TRUE)/n))
+  no_z[i]<-N-n
+  write.csv(te, paste0(i, "bt1break.csv"))
 }
-end_time<-Sys.time()
-total_time<-end_time-start_time
 
-write.csv(result.data, "type1_varycov_3.csv")
-write.csv(stat.data, "type1_varycov_3breakdown.csv")
-write.csv(large_var, "type1_varycov_3largevariance.csv")
-write.csv(no_z, "type1_varycov_3_nonparamnotpossible.csv")
+write.csv(result.data, "bt1.csv")
+write.csv(no_z, "bt1impossible.csv")
